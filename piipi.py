b96c2f8ee62ecc193a9594742fddb579e5ef4d4e
massafragabrielelavoro-cloud <massafra.gabriele.lavoro@gmail.com>
2025-08-20 18:36:27 +0100

    Update piipi.py

--- piipi.py
+++ piipi.py
@@ -3,15 +3,19 @@ import streamlit as st
 # Titolo della pagina
 st.title("❤️🐣Il quiz super importante dei Pipii🐥❤️")
 
-# Domanda
+
+# Titolo e domanda
 st.subheader("Domanda 1:")
 st.write("Qual è stata la prima parola che hai sentito uscire dalla mia voce?")
 
+# Campo di input Streamlit
+risposta1 = st.text_input("Scrivi la tua risposta qui sotto 💬")
 
-# Risposta personalizzata
-risposta1 = input()
-if risposta1 == "Massa":
-    st.success("✅ Bravissima Pipi, ma come hai fatto a ricordartelo?")
-else:
-    st.error(f"❌ Noooooo Pipi, chiu chiu chiu chiu")
+# Controllo della risposta
+if risposta1:
+    if risposta1 == "Massa":
+        st.success("✅ Bravissima Pipi, ma come hai fatto a ricordartelo?")
+    else:
+        st.error("❌ Noooooo Pipi, chiu chiu chiu chiu")
+
 
