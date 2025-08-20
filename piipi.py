cb6e1640e85819e794ad79e05f64220bec70d2ee
massafragabrielelavoro-cloud <massafra.gabriele.lavoro@gmail.com>
2025-08-20 17:40:15 +0100

    Update piipi.py

--- piipi.py
+++ piipi.py
@@ -1,2 +1,8 @@
 titolo = "✨ BENVENUTO NEL MONDO DI PYTHON ✨"
 print(titolo.center(60))
+
+st.markdown("\n <h1 style='text-align: center; color: gold;'>🌟 Quiz Epico di Gabriele 🌟</h1>", unsafe_allow_html=True)
+
+import streamlit as st
+
+st.title("✨ Benvenuto nel tuo quiz storico ✨")
