7bdff577af6244164b1cc24d0b27deeed4e1ce79
massafragabrielelavoro-cloud <massafra.gabriele.lavoro@gmail.com>
2025-08-20 18:20:03 +0100

    Update piipi.py

--- piipi.py
+++ piipi.py
@@ -1,8 +1,125 @@
-titolo = "✨ BENVENUTO NEL MONDO DI PYTHON ✨"
-print(titolo.center(60))
-
-
-
 import streamlit as st
 
-st.title("✨ Benvenuto nel tuo quiz storico ✨")
+# Titolo della pagina
+st.title("❤️🐣Il quiz super importante dei Pipii🐥❤️")
+
+# Domanda
+st.subheader("Domanda 1:")
+st.write("Qual è stata la prima parola che hai sentito uscire dalla mia voce?")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Massa":
+        st.success("✅ Bravissima Pipi, ma come hai fatto a ricordartelo?")
+    else:
+        st.error(f"❌ Noooooo Pipi, chiu chiu chiu chiu")
+
+# Domanda
+st.subheader("Domanda 2:")
+st.write("Come si chiamava il mio bisnonno materno")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Gennaro":
+        st.success("✅ Daleeee Pipiiii wuuuuuuu")
+    else:
+        st.error(f"❌ Ma no pipi, era Gennaro, che praticamente è uguale a dire Ciro. Chiuuuuu!!!")
+
+# Domanda
+st.subheader("Domanda 3:")
+st.write("Ma se io sono pipi pulcino, tu sei pipi come?")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Pulcina":
+        st.success("✅ Beh si, questa era facile ma a volte ho il dubbio")
+    else:
+        st.error(f"❌ Ma Pipi, queste sono le base della pulcinologia")
+
+# Domanda
+st.subheader("Domanda 4:")
+st.write("Chi dovrebbe essere il prossimo Presidente del Consiglio italiano")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Boldrin":
+        st.success("✅ Ottimo, ottimo Pipi, ti meriti un bacino dei pipi anche seui piedini e sulla concha di pipi pulcina")
+    else:
+        st.error(f"❌ Ma orco Dio Pipi, questa era importante")
+
+# Domanda
+st.subheader("Domanda 5:")
+st.write("Cosa accomuna il nostro gattino e il professore di economia politica di Perón in Italia?")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Einaudi":
+        st.success("✅ Wuuuuuuuu, tanto amore per il nostro Eini e le sue sorrelline gattine")
+    else:
+        st.error(f"❌ Mamamamamamamama Pipi, il nostro gattino Einaudi")
+
+# Domanda
+st.subheader("Domanda 6:")
+st.write("Come si chiamava la maga che secondo la leggenda diede il nome alla città di Mantova")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Manto":
+        st.success("✅ Pipi, mi hai stupito, davvero non credevo che te lo ricordassi. Ti amo")
+    else:
+        st.error(f"❌ E vabbè Pipi, questa non è tanto grave non saperla")
+
+# Domanda
+st.subheader("Domanda 7:")
+st.write("Siamo la [...] più bella del mondo")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "coppia":
+        st.success("✅ E ci dispiace per gli altri")
+    else:
+        st.error(f"❌ Maaaa oh pipi, questa pensavo che non ci dovessi nemmeno riflettere")
+
+# Domanda
+st.subheader("Domanda 8:")
+st.write("Se il prezzo per un abbraccio è di 10 bacini e aumenta del 2% ogni mese per 12 mesi; qual sarà il prezzo di un abbraccio dopo un anno? (fermati al secondo decimale) ")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "12,68":
+        st.success("✅ Madonna Pipi, ti ricordi anche la matematica che ti ho insegnato, sono troppo orgoglioso di te")
+    else:
+        st.error(f"❌ Maaaaa Pipi, dopo te lo spiego")
+
+# Domanda
+st.subheader("Domanda 9:")
+st.write("Quanti esami mi mancano per laurearmi?")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "7":
+        st.success("✅ Esatto Pipi, non so se era difficile ma comunque brava")
+    else:
+        st.error(f"❌ Ma chiu Pipi, questo te l'avrò detto un milione di volte")
+
+# Domanda
+st.subheader("Domanda 10:")
+st.write("Qual è il nome sui documenti umani della Pipi più bella di tutto l'Universo e che voglio sposare il prima possibile?")
+
+
+# Risposta personalizzata
+if risposta:
+    if risposta == "Guadalupe":
+        st.success("✅ Ma allora Pipi se lo sai, perché non ci sposiamo????!!!!!!")
+    else:
+        st.error(f"❌ Ma sei tu Pipi, sei tuuuuuuuuuu")
+
